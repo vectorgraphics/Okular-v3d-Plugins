9dad64b8d2d4b42212c0793cc84d4aec8d5f0193
Benjaming Bingham <ben@benbingham.ca>
2024-08-27 12:37:08 -0600

    Added automatic Release creation

---
+++ Release/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+install okularGenerator_poppler.so /usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators
+install okularGenerator_v3d.so /usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators
+
+install fragment.spv /usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators
+install vertex.spv /usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators
