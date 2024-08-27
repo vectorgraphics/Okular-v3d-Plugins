6b68ccbfa8c282abcea0261cc44b68a4329fdb31
Benjaming Bingham <ben@benbingham.ca>
2024-08-27 14:10:32 -0600

    Update install script to work with different linux distros

--- Release/install.sh
+++ Release/install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-install okularGenerator_poppler.so /usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators
-install okularGenerator_v3d.so /usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators
+FEDORA_QT5="/usr/lib64/qt5/plugins/okular/generators"
+UBUNTU_QT5="/usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators"
 
-install fragment.spv /usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators
-install vertex.spv /usr/lib/x86_64-linux-gnu/qt5/plugins/okular/generators
+install okularGenerator_poppler.so $FEDORA_QT5 || \
+install okularGenerator_poppler.so $UBUNTU_QT5 || \
+echo "Could not find installation path for: okularGenerator_poppler.so"
+
+install okularGenerator_v3d.so $FEDORA_QT5 || \
+install okularGenerator_v3d.so $UBUNTU_QT5 || \
+echo "Could not find installation path for: okularGenerator_v3d.so"
+
+install fragment.spv $FEDORA_QT5 || \
+install fragment.spv $UBUNTU_QT5 || \
+echo "Could not find installation path for: fragment.spv"
+
+install vertex.spv $FEDORA_QT5 || \
+install vertex.spv $UBUNTU_QT5 || \
+echo "Could not find installation path for: vertex.spv"
