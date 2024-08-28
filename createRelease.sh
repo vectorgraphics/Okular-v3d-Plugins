8b4195a7ecd9815c8b2d354a06ea7410ee839a1b
Benjaming Bingham <ben@benbingham.ca>
2024-08-28 09:16:01 -0600

    Made release zipping recursive

--- createRelease.sh
+++ createRelease.sh
@@ -7,10 +7,10 @@ cp 3rdParty/V3D-Common/shaders/fragment.spv Release/
 cp ../../../../build/okular/bin/plugins/okular/okularGenerator_poppler.so Release/
 cp ../../../../build/okular/bin/plugins/okular/okularGenerator_v3d.so Release/
 
-zip release.zip                         \
-    Release/vertex.spv                  \
-    Release/fragment.spv                \
-    Release/okularGenerator_poppler.so  \
-    Release/okularGenerator_v3d.so      \
-    Release/install.sh                  \
-    Release/README.md
+zip -r release.zip                          \
+    Release/vertex.spv                      \
+    Release/fragment.spv                    \
+    Release/okularGenerator_poppler.so      \
+    Release/okularGenerator_v3d.so          \
+    Release/install.sh                      \
+    Release/examples
