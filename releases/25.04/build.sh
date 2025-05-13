b800d362cac89b810ee217016be41a0b52dea796
Benjamin Bingham <ben@benbingham.ca>
2025-05-13 14:46:31 -0400

    Fixed build script

--- releases/25.04/build.sh
+++ releases/25.04/build.sh
@@ -2,4 +2,4 @@
 
 cmake -S okular/ -B build/ --install-prefix $PWD/usr
 cmake --build build/ --target okularGenerator_v3d
-cmake --build build/ --target okularGenerator_pdf
+cmake --build build/ --target okularGenerator_poppler
