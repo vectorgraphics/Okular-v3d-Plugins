044e86112e601d49a0a627c8dfeddc7ce0bf4e36
Benjamin Bingham <ben@benbingham.ca>
2025-05-15 14:14:51 -0400

    Added an archlinux test

--- testing/default-home/install.sh
+++ testing/default-home/install.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-FEDORA_QT6="/usr/lib64/qt6/plugins/okular_generators/"
-UBUNTU_QT6="/usr/lib/x86_64-linux-gnu/qt6/plugins/okular_generators/"
+FEDORA="/usr/lib64/qt6/plugins/okular_generators/"
+UBUNTU="/usr/lib/x86_64-linux-gnu/qt6/plugins/okular_generators/"
+ARCH="/usr/lib64/qt6/plugins/okular_generators"
 
-install okularGenerator_poppler.so $FEDORA_QT6 2> /dev/null || \
-install okularGenerator_poppler.so $UBUNTU_QT6 2> /dev/null || \
+install okularGenerator_poppler.so $FEDORA 2> /dev/null || \
+install okularGenerator_poppler.so $UBUNTU 2> /dev/null || \
+install okularGenerator_poppler.so $ARCH 2> /dev/null || \
 echo "Could not find installation path for: okularGenerator_poppler.so"
 
-install okularGenerator_v3d.so $FEDORA_QT6 2> /dev/null || \
-install okularGenerator_v3d.so $UBUNTU_QT6 2> /dev/null || \
+install okularGenerator_v3d.so $FEDORA 2> /dev/null || \
+install okularGenerator_v3d.so $UBUNTU 2> /dev/null || \
+install okularGenerator_v3d.so $ARCH 2> /dev/null || \
 echo "Could not find installation path for: okularGenerator_v3d.so"
 
-install fragment.spv $FEDORA_QT6 2> /dev/null || \
-install fragment.spv $UBUNTU_QT6 2> /dev/null || \
+install fragment.spv $FEDORA 2> /dev/null || \
+install fragment.spv $UBUNTU 2> /dev/null || \
+install fragment.spv $ARCH 2> /dev/null || \
 echo "Could not find installation path for: fragment.spv"
 
-install vertex.spv $FEDORA_QT6 2> /dev/null || \
-install vertex.spv $UBUNTU_QT6 2> /dev/null || \
+install vertex.spv $FEDORA 2> /dev/null || \
+install vertex.spv $UBUNTU 2> /dev/null || \
+install vertex.spv $ARCH 2> /dev/null || \
 echo "Could not find installation path for: vertex.spv"
 
 install v3d-mime.xml /usr/share/mime/packages 2> /dev/null || \
