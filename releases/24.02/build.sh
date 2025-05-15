2dfd42413eda73fdef24f01de1a8d3d723b55bbb
Benjamin Bingham <ben@benbingham.ca>
2025-05-15 12:40:43 -0400

    Added build scripts that allow for either release or debug or both

--- releases/24.02/build.sh
+++ releases/24.02/build.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 
-if [[ $1 = "--clean" ]] || [[ $1 = "--clean-only" ]]; then
+clean=0
+cleanOnly=0
+release=0
+debug=0
+
+for arg in $@
+do
+    if [[ $arg = "--clean" ]]; then
+        clean=1
+    fi
+
+    if [[ $arg = "--clean-only" ]]; then
+        cleanOnly=1
+    fi
+
+    if [[ $arg = "--release" ]]; then
+        release=1
+    fi
+
+    if [[ $arg = "--debug" ]]; then
+        debug=1
+    fi
+done
+
+if [[ $clean -eq 1 ]] || [[ $cleanOnly -eq 1 ]]; then
     rm -r build/
+    rm -r usr/
 fi
 
-if [[ $1 = "--clean-only" ]]; then
+if [[ $cleanOnly -eq 1 ]]; then
     exit 0
 fi
 
-cmake -S okular/ -B build/ --install-prefix $PWD/usr
-cmake --build build/ --target okularGenerator_v3d
-cmake --build build/ --target okularGenerator_poppler
+if [[ $release -eq 0 ]] && [[ $debug -eq 0 ]]; then
+    echo "No build type selected, please specify either --debug, --release or both."
+    exit 1
+fi
+
+if [[ $debug -eq 1 ]]; then
+    cmake -S okular/ -B build/debug/ -DCMAKE_BUILD_TYPE=Debug --install-prefix $PWD/usr/debug/
+    cmake --build build/debug/ --target okularGenerator_v3d
+    cmake --build build/debug/ --target okularGenerator_poppler
+fi
+
+if [[ $release -eq 1 ]]; then
+    cmake -S okular/ -B build/release/ -DCMAKE_BUILD_TYPE=Release --install-prefix $PWD/usr/release
+    cmake --build build/release/ --target okularGenerator_v3d
+    cmake --build build/release/ --target okularGenerator_poppler
+fi
+
