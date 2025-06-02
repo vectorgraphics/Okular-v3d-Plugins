d53585d370b590a05aaabbe0df9b7fb983c4ac8e
Benjamin Bingham <ben@benbingham.ca>
2025-06-02 11:37:57 -0400

    Fixed bug with mouse releasing

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "It is highly recomended to execute this script inside of a fedora40 vm/distrobox in order to ensure the highest level of compatibility"
+
 for d in */ ; do
     echo "========== Building version "$(basename ${d})" =========="
     cd $d && sh build.sh $@ && cd ../
