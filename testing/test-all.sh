e756daaefa641c289f9dba32db71ec76ad6f455e
Benjamin Bingham <ben@benbingham.ca>
2025-06-05 09:44:43 -0400

    Revamped testing script

--- testing/test-all.sh
+++ testing/test-all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 for d in */ ; do
-    echo "========== Starting "$(basename ${d})}" test =========="
-    cd $d && sh test.sh $@ && cd ../
+    echo "========== Starting "$(basename ${d})" test =========="
+    cd $d
+    sh test.sh $@
+    cd ../
 done
