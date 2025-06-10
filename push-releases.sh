c089cd81ae0ee02064b1da297692e6abfc1cf69f
Benjamin Bingham <ben@benbingham.ca>
2025-06-10 09:37:04 -0400

    Added --help to push-releases.sh

--- push-releases.sh
+++ push-releases.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+for arg in $@
+do
+    if [[ $arg = "--help" ]]; then
+        echo "Usage: ./push-releases.sh <Release Number> <\"Release Notes\"> [OPTIONS]..."
+        echo "Pushes already built releases to github using the github CLI"
+        echo "Be sure to specify the release number and release notes properly"
+        echo "  --help          See this message"
+
+        exit
+    fi
+done
+
 if [[ ! -d "releases" ]]; then
     echo "No releases to push!"
     echo "Create releases with create-releases.sh"
