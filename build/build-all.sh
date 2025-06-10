417a7ac3fa4052463c5d2d492da0a7e8158912a1
Benjamin Bingham <ben@benbingham.ca>
2025-06-10 09:51:13 -0400

    Specified qt6 packages should be downgraded

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "It is highly recomended to execute this script inside of a fedora40 vm/distrobox in order to ensure the highest level of compatibility"
+echo "It is highly recomended to execute this script inside of a fedora40 vm/distrobox with all of the qt6 packages downgraded in order to ensure the highest level of compatibility"
 
 for d in */ ; do
     echo "========== Building version "$(basename ${d})" =========="
