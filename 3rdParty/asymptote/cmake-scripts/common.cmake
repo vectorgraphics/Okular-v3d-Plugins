3fc47f622ab5060ccac83ad43f982ced2f758184
Benjamin Bingham <94578530+Ben-Bingham@users.noreply.github.com>
2026-05-13 08:21:46 -0600

    Removed asy

--- 3rdParty/asymptote/cmake-scripts/common.cmake
+++
@@ -1,6 +0,0 @@
-macro(TODO_NOTIMPL message)
-    message(FATAL_ERROR "TODO: ${message}")
-endmacro()
-
-set(cmake_release_build_types Release RelWithDebInfo MinSizeRel)
-set(cmake_debug_build_types Debug)
--- 3rdParty/asymptote/tests/imp/access.asy
+++
@@ -1,6 +0,0 @@
-import TestLib;
-
-StartTest('dotted directories, no "as"');
-access imp.imports.A;
-assert(A.B.x == 4);
-EndTest();
