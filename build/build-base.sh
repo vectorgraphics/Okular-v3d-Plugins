ad65b368f8574513e3cae56c68745524bd546c9d
Ben Bingham <ben@benbingham.ca>
2026-08-11 09:17:35 -0600

    Streamlined building process

--- build/build-base.sh
+++ build/build-base.sh
@@ -43,6 +43,7 @@ buildFunc() {
     if [[ $clean -eq 1 ]] || [[ $cleanOnly -eq 1 ]]; then
         rm -rf build/
         rm -rf usr/
+        rm -rf stage/
     fi
 
     if [[ $cleanOnly -eq 1 ]]; then
@@ -65,5 +66,44 @@ buildFunc() {
         cmake --build build/release/ --target v3dCommon
         cmake --build build/release/ --target okularGenerator_v3d
         cmake --build build/release/ --target okularGenerator_poppler
+    fi
+
+    # ---- Staging phase ----
+    # Clean the staging directory at the start of each build run
+    rm -rf "stage/"
+    mkdir -p "stage/"
+
+    # Determine which build tree to stage from (prefer release, fall back to debug)
+    BUILD_TREE="build/release"
+    if [[ $release -eq 0 ]] && [[ $debug -eq 1 ]]; then
+        BUILD_TREE="build/debug"
+    fi
+
+    BIN_DIR="${BUILD_TREE}/bin"
+
+    for so_file in okularGenerator_v3d.so okularGenerator_poppler.so; do
+        if [[ -f "${BIN_DIR}/okular_generators/${so_file}" ]]; then
+            cp "${BIN_DIR}/okular_generators/${so_file}" "stage/"
+        fi
+    done
+
+    if [[ -f "${BIN_DIR}/libv3dCommon.so" ]]; then
+        cp "${BIN_DIR}/libv3dCommon.so" "stage/"
+    fi
+
+    if [[ -d "../../asymptote/base/shaders" ]]; then
+        cp ../../asymptote/base/shaders/*.glsl "stage/"
+    fi
+
+    if [[ -f "../../base-release/install.sh" ]]; then
+        cp "../../base-release/install.sh" "stage/"
+    fi
+
+    if [[ -f "../../base-release/v3d-mime.xml" ]]; then
+        cp "../../base-release/v3d-mime.xml" "stage/"
+    fi
+
+    if [[ -f "../../base-release/okularApplication_v3d.desktop" ]]; then
+        cp "../../base-release/okularApplication_v3d.desktop" "stage/"
     fi
 }
--- testing/test-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for d in */ ; do
-    echo "========== Starting "$(basename ${d})" test =========="
-    cd $d
-    sh test.sh $@
-    cd ../
-done
